4ec633c50505c215ad3294fe1a03aad4bf33daa2
manchenlee <45174916+manchenlee@users.noreply.github.com>
2026-02-04 12:40:33 +0800

    Delete run_test.ps1

--- run_test.ps1
+++
@@ -1,4 +0,0 @@
-$h = python -c "from config import settings; print(settings.SERVER_HOST)".Trim()
-$p = python -c "from config import settings; print(settings.SERVER_PORT)".Trim()
-Write-Host "Stress Testing: http://${h}:${p}" -ForegroundColor Cyan
-k6 run -e HOST=$h -e PORT=$p --out web-dashboard --summary-trend-stats="avg,min,med,max,p(50),p(95),p(99),p(99.9)" --http-debug="full" script.js
